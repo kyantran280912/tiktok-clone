719db2ea07a2500f4bf2b45215bd7c52a970cca6
Tran Van Binh <binh280912@gmail.com>
2022-09-16 18:21:30 +0700

    push

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
-import images from "~/assets/images"
+import images from '~/assets/images';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 const Header = () => {
     return (
         <StyledHeader>
             <div className="inner">
                 <div className="logo">
-                   <img src={images.logo} alt="Tiktok" />
+                    <img src={images.logo} alt="Tiktok" />
                 </div>
                 <div className="search">
-                    <input type="text" placeholder='Search accounts and video' spellCheck={false} />
-                    <button>
-                        {/* Icon */}
+                    <input type="text" placeholder="Search accounts and video" spellCheck={false} />
+                    <button className="clear">
+                        <FontAwesomeIcon icon={faCircleXmark} />
                     </button>
-                    {/* Loading */}
-                    <button>
-                        {/* search  */}
+                    <FontAwesomeIcon className="loading" icon={faSpinner} />
+                    <button className="search-btn">
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
                     </button>
                 </div>
+                <div className="action"></div>
             </div>
         </StyledHeader>
     );
@@ -25,11 +28,77 @@ const Header = () => {
 const StyledHeader = styled.header`
     height: 60px;
     box-shadow: 0px 1px 1px rgb(0 0 0 / 12%);
+    --search-boder-radius: 92px;
+    --search-height: 46px;
+    --search-top-spacer: 9px;
     .inner {
         height: 100%;
         max-width: var(--default-layout);
-        margin-left: auto;
-        margin-right: auto;
+        margin: 0 auto;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    /* .logo {
+    } */
+    .search {
+        width: 361px;
+        height: var(--search-height);
+        background-color: rgba(22, 24, 35, 0.06);
+        border-radius: var(--search-boder-radius);
+        position: relative;
+        padding-left: 16px;
+        display: flex;
+        border: 1.5px solid transparent;
+
+        input {
+            font-size: 1.6rem;
+            height: 100%;
+            background-color: transparent;
+            flex: 1;
+            caret-color: var(--pri);
+        }
+        input:not(:placeholder-shown) ~ .search-btn{
+            color: rgba(22, 24, 35, 0.75)
+        }
+        ::after {
+            content: '';
+            position: absolute;
+            top: var(--search-top-spacer);
+            right: 52px;
+            height: calc(var(--search-height) - var(--search-top-spacer) * 2);
+            width: 1px;
+            background-color: rgba(22, 24, 35, 0.12);
+        }
+        &:focus-within {
+            border-color: rgba(22, 24, 35, 0.2);
+        }
+    }
+    .clear,
+    .loading {
+        position: absolute;
+        top: 50%;
+        transform: translateY(-50%);
+        right: calc(52px + 10px);
+        color: rgba(22, 24, 35, 0.34);
+    }
+    .search-btn {
+        width: 52px;
+        height: 100%;
+        border-top-right-radius: var(--search-boder-radius);
+        border-bottom-right-radius: var(--search-boder-radius);
+        cursor: pointer;
+        font-size: 1.6rem;
+        color: rgba(22, 24, 35, 0.34);
+        &:hover {
+            background-color: rgba(22, 24, 35, 0.03);
+        }
+        :active{
+            background-color: rgba(22, 24, 35, 0.06);
+
+        }
+    }
+    .action {
     }
 `;
 export default Header;
